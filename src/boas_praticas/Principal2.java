7e9323cc8474a60a6c29d59bbd137e6aaa77136a
silva3265 <silva3265@gmail.com>
2024-06-18 23:25:10 -0300

    Boas práticas: evite float e double se precisão é importante

---
+++ src/boas_praticas/Principal2.java
@@ -0,0 +1,17 @@
+package boas_praticas;
+
+public class Principal2 {
+
+    public static void main(String[] args) {
+        ContaCorrente conta = new ContaCorrente();
+        conta.depositar(1.0);
+
+        conta.sacar(0.10);
+        conta.sacar(0.20);
+        conta.sacar(0.30);
+        conta.sacar(0.40);
+
+        System.out.println(conta.getSaldo());
+    }
+
+}
